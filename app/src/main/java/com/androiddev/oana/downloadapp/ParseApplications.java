a2ab6683d01baafdcfeb910b0f7436500d887bf4
Oana Patrascu <oanapat@users.noreply.github.com>
2016-07-11 17:45:45 -0700

    Update ParseApplications.java

--- app/src/main/java/com/androiddev/oana/downloadapp/ParseApplications.java
+++ app/src/main/java/com/androiddev/oana/downloadapp/ParseApplications.java
@@ -12,15 +12,12 @@ import java.util.ArrayList;
  * Created by Oana on 7/11/2016.
  */
 public class ParseApplications {
-    //store the data, the xml that we will process and pass data to this class, ParseApplication
     private String xmlData;
-    //use the array list to store multiple elements and all the entry stored in the array will be of type application
+    //use the array list to store multiple elements
     private ArrayList<Application> applications;
 
-    //generate a constructor and add getters for the application
     public ParseApplications(String xmlData) {
         this.xmlData = xmlData;
-        //initialize it
         applications = new ArrayList<Application>();
     }
     //return the data when finished
@@ -35,25 +32,21 @@ public class ParseApplications {
         boolean inEntry = false;
         String textValue = "";
 
-        //use build in code to extract data from xml files
         try{
-            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();//create new instance of this class so we can access the new features
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
             factory.setNamespaceAware(true);
             XmlPullParser xpp = factory.newPullParser();
-            xpp.setInput(new StringReader(this.xmlData));//parsing the code that is been send to us and that we are going to work on. Needs code of a specific form which is achieved by using the StringReader to parse
+            xpp.setInput(new StringReader(this.xmlData));
             //capture the event type. each time we get to a particular part of the xml file the event type will change.
-            //every time we get to an entry we need to initialize the application object
             int eventType = xpp.getEventType();
             //continue looping through the xml file until we get to the end of the file
             while(eventType != XmlPullParser.END_DOCUMENT){
                 //get the name of the tag, start with the START_TAG
                 String tagName = xpp.getName();
-                //depending on the tag name we will start to do some processing
                 switch(eventType){
                     case XmlPullParser.START_TAG:
                         //display this message
                         Log.d("ParseApplication", "Starting tag for "+ tagName);
-                        //checking if we are at the start and if yes we will start to process an application
                         if(tagName.equalsIgnoreCase("entry")){
                             inEntry = true;
                             currentRecord = new Application();
@@ -68,10 +61,7 @@ public class ParseApplications {
 
                     case XmlPullParser.END_TAG:
                         Log.d("ParseApplication", "Ending tag for "+ tagName);
-                        //only if we are actually withing the entry component of the xml file
                         if(inEntry){
-                            //if you reached the end of an entry we will save the record and set inEntry to false so that we can process another record
-                            //these are test cases that keep the program going
                             if(tagName.equalsIgnoreCase("entry")){
                                 applications.add(currentRecord);
                                 inEntry = false;
@@ -88,7 +78,6 @@ public class ParseApplications {
                         }
                         break;
                     default:
-                        //Nothing else to do.
                 }
                 eventType = xpp.next();
             }
@@ -99,7 +88,6 @@ public class ParseApplications {
 
         }
         //summary of what is being processed
-        //go throigh each elem that is in the arraylist and for each element create an object called app
         for(Application app : applications){
             Log.d("ParseApplications", "*****************");
             Log.d("ParseApplications", "Name: " + app.getName());
